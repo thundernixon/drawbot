45a8a50c24a5d323e0f1c29162332dd4a9b97fa5
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-09-14 20:10:00 +0200

    refine animation

--- animation.py
+++ animation.py
@@ -4,9 +4,15 @@ frames = 50
 
 width = 50
 
+blue = 0
+red = 0
+
 for frame in range( frames ):
-    width += 2
+    blue += 0.01
+    red += 0.015
+    width += 1
     newPage()
+    fill(red,0,blue)
     rect(100,100,width,100)
 
 saveImage('test.gif')
