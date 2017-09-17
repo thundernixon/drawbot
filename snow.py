3216160fda03b3771550dd7265397d0b1adc5cc6
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-09-17 10:30:58 +0200

    clean up and comment snow

--- snow.py
+++ snow.py
@@ -9,17 +9,14 @@ size(500,500);
 #                 maxValue=25))
 #     ], globals())
 
-# fill(0,0,.15);
+# gradient for night sky
 def makeSky():
     linearGradient((250,0),(250,500), ([0,0,.5], [0,0,.15]))
 
 makeSky();
 rect(0,0,500,500);
 
-
-# fill(1,1,1)
-# rect(0,0,500,40)
-
+# function to make "mountain range"
 def printMountains(x,y,w,h):
     rotate(45)
     translate(0,-20)
@@ -27,17 +24,14 @@ def printMountains(x,y,w,h):
     rect(x,y, w, h)
     rotate(-45)
 
-
+# function to start mountain-making
 def makeMountains():
     fill(1,1,1)
     rect(0,0,500,40)
     for m in range( 20 ):
         printMountains(m*20,m, randint(100,150),randint(100,150));
 
-
-
-snowSize = 10
-
+# function to make "Snow"
 def printSnow(x, y, alpha, rotation, snowSize):
     fill(1,1,1,alpha);
     rotate(rotation*22.5)
@@ -45,22 +39,22 @@ def printSnow(x, y, alpha, rotation, snowSize):
     rect(x - 1, y - snowSize/2, 2, snowSize)
     rotate(-rotation*22.5)
 
-
-
+# function to start snow-making
 def makeItSnow():
     for i in range(300):
         snowSize = randint(2,12);
         printSnow(random()*500,random()*500, random(), random(), snowSize)
 
+# run starts snow-starting function
 makeItSnow();
 
+# run starts mountain-starting function
 makeMountains();
-# mountains = makeMountains();
 
-
-
+# sets up number of frames for animation
 frames = 20
 
+# creates each frame, cueing snow and mountains
 for frame in range( frames ):
     newPage()
     # fill(0,0,.15);
@@ -70,6 +64,5 @@ for frame in range( frames ):
     makeMountains(); # can I make a mountain range once, and repeat that in every frame, while still having random snowflakes? Something like defining a mountain range "object" once, then just printing the same one on each new frame...
     # print mountains;
     
-
-
+# saves gif of animation
 saveImage('mountains.gif')
