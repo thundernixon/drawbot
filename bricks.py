ba84e730ab227bfe6da51df5ff1c4fc0f5ec045e
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-09-19 23:29:38 +0200

    set off 1 of every 6 bricks in random color

--- bricks.py
+++ bricks.py
@@ -3,22 +3,30 @@ width = 1000
 rows = 20
 cols = 10
 
+fill(0,0.1,.3)
+rect(0,0,width, height)
+
 # for each row of height/20, 
 # make 10 bricks, and
 # add width/10 to the x-coordinate each time
 # then move to next row
 
-stroke(1,0,0)
+# stroke(1,0,0)
 
 def brickRow(x, y):
     for col in range(cols):
+        if randint(1,6) == 1:
+            print x, y
+            fill(0,1,0)
+        else:
+            fill(0,0.1,.3)
         rect(x, y, (width/cols), (height/rows))
         x += (width/cols)
-        print x
-
-# brickRow(0);
 
 for row in range(rows):
     brickRow(0, row*(height/rows))
+
+for i in range(12):
+    print randint(1,6)
     
 # each brick has a 1 in 6 chance of being a color
