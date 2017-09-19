9663025136c18b502023030155e7c6263ba8a86c
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-09-19 23:17:38 +0200

    cover canvas in "bricks"

--- bricks.py
+++ bricks.py
@@ -1,4 +1,24 @@
-rect(100,100,100,100)
+height = 1000
+width = 1000
+rows = 20
+cols = 10
 
-rect(300,300,100,100) # adding a new rectangle
-rect(300,300,100,100) # adding a new rectangle
+# for each row of height/20, 
+# make 10 bricks, and
+# add width/10 to the x-coordinate each time
+# then move to next row
+
+stroke(1,0,0)
+
+def brickRow(x, y):
+    for col in range(cols):
+        rect(x, y, (width/cols), (height/rows))
+        x += (width/cols)
+        print x
+
+# brickRow(0);
+
+for row in range(rows):
+    brickRow(0, row*(height/rows))
+    
+# each brick has a 1 in 6 chance of being a color
