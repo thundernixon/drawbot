0abca39b5a22ff1a73faccab0032f683fce3a4b3
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-09-20 16:17:41 +0200

    add semi-randomly positioned "yo"s

--- class-lessons/loops-092017.py
+++ class-lessons/loops-092017.py
@@ -9,29 +9,24 @@ squares = 12
 gridDistance = 162
 squareSize = 120
 
-frames = 6
+frames = 50
 
 for frame in range(frames):
     currentFrame = frame +1 # get number of current frame
     # set up black page
     newPage(w,h)
-    frameDuration(.25)
+    frameDuration(.125)
     fill(0,0,0)
     rect(0,0,1000,1000)
     ## make condition for number of squares
-    if frame >= (frames / 2): # if current frame is over half of total
-        # decreasingFrame = (frame - 1) - (frames // 2) # double divide gives and integer
-        squares = currentFrame - (frames // 2)
-            # frame 4 minus 1 is 3 squares
-            # frame 5 minus 3  is 2 squares
-            # frame 6 minus 5 is 1 square
-        print "The frame is " + str(frame + 1) + " and there are " + str(squares) + " squares"
+    if currentFrame > (frames // 2): # if current frame is over half of total
+        squares = squares - 1 # take current number of squares and shrink by one for each frame
+        print "The frame is " + str(currentFrame) + " and there are " + str(squares) + " squares"
     else:
-        squares = currentFrame
-    
-    fill(1,1,1)
-    fontSize(30)
-    text("frame " + str(currentFrame), (20, 40))
+        squares = currentFrame # take current number of squares and grow by one for each frame
+    # fill(1,1,1)
+    # fontSize(30)
+    # text("frame " + str(currentFrame), (20, 40))
     for i in range(squares):
         squareWidth = w/((squares * 2)+1)
         x = squareWidth + (i * squareWidth*2)
@@ -46,4 +41,4 @@ for frame in range(frames):
     # if frame > frames/2, 
     # squares = frame - (frames/2 + frame)
 
-# saveImage('../gifs/checkerboard.gif')
+saveImage('../gifs/checkerboard-alt.gif')
