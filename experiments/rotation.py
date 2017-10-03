2dedb7fba46435b6df59253adef61a05929c852e
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-10-03 10:22:23 +0200

    rotational weirdness

--- experiments/rotation.py
+++ experiments/rotation.py
@@ -7,27 +7,31 @@ Variable([
                 value=0,
                 minValue=0,
                 maxValue=720)),
-    dict(name="numberOfOvals", ui="Slider",
+    dict(name="ovalsPerRow", ui="Slider",
             args=dict(
                 # some vanilla specific
                 # setting for a slider
                 value=1,
                 minValue=1,
-                maxValue=100))
+                maxValue=40))
     ], globals())
 
 canvasWidth = 1000
 canvasHeight = canvasWidth
 ovals = 5
 
-rotate(rotation)
-# translate(x=canvasWidth/2, y=canvasWidth/2)
 
-ovals = int(numberOfOvals)
+
+
+ovals = int(ovalsPerRow)
 
 for i in range(0,ovals):
+    # fill(1,1,.5)
+    # rect(5,5,canvasWidth, canvasHeight)
     ovalSize = canvasWidth/((ovals * 2)+1)
     x = ovalSize + (i * ovalSize*2)
     for j in range(0,ovals):
+        translate(canvasWidth/2, canvasWidth/2)
+        rotate(rotation)
         y = ovalSize + (j * ovalSize * 2)
         oval(x,y, ovalSize, ovalSize)
