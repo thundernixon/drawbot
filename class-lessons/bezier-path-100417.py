27b793c2ecb16fcf43b40bf80c0400a61cf4870b
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-10-05 11:04:54 +0200

    updated bezier

--- class-lessons/bezier-path-100417.py
+++ class-lessons/bezier-path-100417.py
@@ -1,12 +1,15 @@
 fill(.75)
 rect(0,0,1000,300)
 
+# save()
+# translate(x=0, y=-50)
+
 bez = BezierPath()
 
 # big shape
 bez.moveTo((200, 650))
 bez.curveTo(
-    (200, 850), # handle one
+    (200, 888), # handle one
     (428, 900), # handle two
     (500, 900) # endpoint
     )
@@ -15,35 +18,77 @@ bez.curveTo(
     (800, 750), # handle two
     (800, 650) # endpoint
     )
-bez.lineTo((800, 200))
+bez.lineTo((800, 300))
+bez.curveTo(
+    (800, 275), # handle one
+    (800, 240), # handle two
+    (850, 240) # endpoint
+    )
+bez.lineTo((850, 200))
 bez.lineTo((600, 200))
 bez.lineTo((600, 650))
 bez.curveTo(
     (600, 699), # handle one
     (585, 750), # handle two
-    (500, 750) # endpoint
+    (492, 750) # endpoint
     )
 bez.curveTo(
     (426, 749), # handle one
-    (400, 712), # handle two
-    (400, 650) # endpoint
+    (392, 712), # handle two
+    (392, 650) # endpoint
     )
 bez.closePath()
 
 # //// bowl /////
 
-bez.moveTo((625, 550))
-bez.lineTo((400, 500))
+bez.moveTo((600, 576))
+bez.lineTo((406, 540))
 bez.curveTo(
-    (600, 699), # handle one
-    (585, 750), # handle two
-    (500, 750) # endpoint
+    (268, 511), # handle one
+    (154, 472), # handle two
+    (160, 326) # endpoint
     )
+bez.curveTo(
+    (160, 179), # handle one
+    (312, 178), # handle two
+    (348, 178) # endpoint
+    )
+bez.curveTo(
+    (468, 178), # handle one
+    (552, 226), # handle two
+    (600, 300) # endpoint
+    )
+bez.lineTo((600, 402))
+bez.curveTo(
+    (520, 308), # handle one
+    (466, 296), # handle two
+    (418, 296) # endpoint
+    )
+bez.curveTo(
+    (392, 296), # handle one
+    (364, 312), # handle two
+    (364, 346) # endpoint
+    )
+bez.curveTo(
+    (364, 376), # handle one
+    (362, 412), # handle two
+    (458, 438) # endpoint
+    )
+bez.lineTo((600, 472))
+bez.closePath()
+
+bez.moveTo((200, 125))
+bez.lineTo((800, 125))
+bez.lineTo((800, 75))
+bez.lineTo((200, 75))
+bez.closePath()
 
 fill(1,0,0)
 stroke(0)
 strokeWidth(10)
 drawPath(bez)
+
+
 
 print bez.pointInside((534, 830))
 
@@ -63,3 +108,5 @@ print bez.bounds()
 x,y,cx,cy = (bez.bounds())
 
 rect(x,y,cx,cy)
+
+# restore()
