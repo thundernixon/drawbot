5558def133360275808be80311604366a32e24e6
Stephen Nixon <swr410@gmail.com>
2017-11-01 16:55:57 +0100

    get font weights working on a repeating pattern

--- experiments/type_alphabet_weight_test-110117.py
+++ experiments/type_alphabet_weight_test-110117.py
@@ -1,26 +1,57 @@
 import string
 
+w = 1000
+h = 1000
+
 alpha = string.lowercase
 
 # print alpha
 
 font("Stroop")
 fontSize(50)
+fallbackFont("Input Mono")
 
 counter = 1
 
+#for each character in string, repeat three times in new string
+
+# def generateNewString(string):
+#     newString = ""
+#     for char in string:
+#         newString += char*3
+#     return newString
+        
+
 def testWeights(string):
     counter = 1
+    # generateNewString(string)
+    newString = ""
     for char in string:
+        newString += char*3
+    
+    for char in newString:
         print char
         # text(char, (50*counter, 100))
-        text(char, (100, 100))
+        if counter % 3 == 0:
+            fill(1,0,0) # red
+            font("Times New Roman Bold")
+        elif (counter + 1) % 3 ==0:
+            fill(0,1,.5) # teal
+            font("Times New Roman")
+        else:
+            fill(0,0,1) # blue
+            font("Times New Roman Italic")
+        
+        
+        
+        text(char, (100, 800))
         letterWidth, letterHeight = textSize(char)
         translate(letterWidth,0)
         print letterWidth
         # print textSize(char)
-        # counter += 1
+        counter += 1
         
         # if letter doesn't exist, replace with n
 
 testWeights(alpha)
+# generateNewString(alpha)
