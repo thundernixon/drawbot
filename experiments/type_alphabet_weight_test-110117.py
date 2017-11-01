cb1409cb0b42480295925db0bb024ef02de46220
Stephen Nixon <swr410@gmail.com>
2017-11-01 18:23:46 +0100

    iterate fonts but still have fallback inserting

--- experiments/type_alphabet_weight_test-110117.py
+++ experiments/type_alphabet_weight_test-110117.py
@@ -1,15 +1,20 @@
 import string
 
+size('A3')
+
 w = 1000
 h = 1000
+
+# set canvas to a3 size
 
 alpha = string.lowercase
 
 # print alpha
 
 font("Stroop")
-fontSize(50)
+fontSize(100)
 fallbackFont("Input Mono")
+
 
 counter = 1
 
@@ -24,31 +29,44 @@ counter = 1
 
 def testWeights(string):
     counter = 1
+    lineCount = 1
+    textWidth = 0
+    starterPosX = 50
+    starterPosY = 100
+    positionX = starterPosX
+    positionY = height()-150
     # generateNewString(string)
     newString = ""
     for char in string:
         newString += char*3
     
     for char in newString:
-        print char
-        # text(char, (50*counter, 100))
         if counter % 3 == 0:
-            fill(1,0,0) # red
-            font("Times New Roman Bold")
+            # fill(1,0,0) # red
+            font("Stroop Extrabold")
         elif (counter + 1) % 3 ==0:
-            fill(0,1,.5) # teal
-            font("Times New Roman")
+            # fill(0,1,.5) # teal
+            font("Stroop")
         else:
-            fill(0,0,1) # blue
-            font("Times New Roman Italic")
+            # fill(0,0,1) # blue
+            font("Stroop Sans Book")
         
         
         
-        text(char, (100, 800))
+        text(char, (positionX, positionY)) # to do: set this equal to something based on font size
         letterWidth, letterHeight = textSize(char)
-        translate(letterWidth,0)
-        print letterWidth
-        # print textSize(char)
+        
+        textWidth += letterWidth
+        positionX += letterWidth
+        
+        if counter % 9 == 0: # to do: set this equal to something based on overall text width
+            # translate(100-textWidth,-letterHeight)
+            lineCount += 1
+            positionX = starterPosX
+            positionY -= starterPosY
+            print lineCount
+        # else:
+            # translate(letterWidth,0)
         counter += 1
         
         # if letter doesn't exist, replace with n
