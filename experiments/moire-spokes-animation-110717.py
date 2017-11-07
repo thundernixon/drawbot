ece1509f4bfd355d8720ddc9acb647f6446ed47e
Stephen Nixon <swr410@gmail.com>
2017-11-08 00:28:22 +0100

    moire 20

--- experiments/moire-spokes-animation-110717.py
+++ experiments/moire-spokes-animation-110717.py
@@ -4,55 +4,58 @@ def spokedCircle(x, y, radius, centerRadius, spokes, spokeWidth):
     translate(x, y)
     for spoke in range(spokes):
         # fill(0 + 1/spokes,0,0)
-        fill(randint(750,900)*.001,randint(500,686)*.001,0) #.68627451, .588235294 
+        fill(randint(750,900)*.001,randint(500,686)*.001,.25) #.68627451, .588235294 
         rect(0,centerRadius,spokeWidth,radius-centerRadius)
         rotate(360/spokes)
     restore()
-    
-
-distance = 240
-    
-radius = 928
-centerRadius = 500
-spokes = 21
 
 W, H = 1000, 1000
 
 frames = 20 # currently must be lower than number of maxSpokes
 rotations = 1
 totalSize = 1000
-maxSpokes = 65 # currently must be higher than number of frames
+spokes = 65
+maxSpokes = 75 # currently must be higher than number of frames
 minSpokes = 20
 
-distance = 50
-maxDistance = 950
+distance = 0
+maxDistance = 1000
 
-radius = totalSize
-centerRadius = -totalSize
+starterRadius = 1450
+starterCenterRadius = 50
+
+radius = starterRadius
+centerRadius = starterCenterRadius
     
+incrementDistance = (maxDistance - distance)/frames
+incrRadius = ((totalSize-starterRadius)/frames)*3.5
+incrCenterRadius = ((totalSize-starterCenterRadius)/frames)*.25
+
 for i in range(frames):
     newPage(W, H)
-    incrementDistance = (maxDistance - distance)/frames
-    incrRadius = (totalSize/frames)
+
     if i < frames/2:
         # print "going up " + str(i*((maxDistance - distance)/frames))
         
         distance += incrementDistance
         radius -= incrRadius
-        centerRadius += incrRadius*2
-        print distance
+        centerRadius += incrCenterRadius
+        spokes += 3
+        print incrRadius, incrCenterRadius
+        print radius, centerRadius
     else:
         # print "going down " + str(i*((maxDistance - distance)/frames))
         # distance -= i*((maxDistance - distance)/frames)
         distance -= incrementDistance
         radius += incrRadius
-        centerRadius -= incrRadius*2
-        print distance
+        centerRadius -= incrCenterRadius
+        spokes -= 3
+        print radius, centerRadius
     fill(0.1,0.1,0.1)
     rect(0,0,1000,1000)
     # radius = totalSize - i*(totalSize/frames)
     # centerRadius = -totalSize + i*(totalSize/frames)*2
-    spokes = maxSpokes
+    # spokes = maxSpokes
     # spokes = minSpokes + i*int(((maxSpokes - minSpokes)/frames)) #increment up
     # spokes = maxSpokes - i*int(round(((maxSpokes - minSpokes)/frames)))
     # print spokes
@@ -66,4 +69,4 @@ for i in range(frames):
 
     # translate(-500,-500+centerRadius)
 
-# saveImage("exports/moire_13-110717.gif")
+# saveImage("exports/moire_18-110717.gif")
