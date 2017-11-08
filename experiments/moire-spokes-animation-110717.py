8f5cef938e585fbd976629afed36e0db4b420ecc
Stephen Nixon <swr410@gmail.com>
2017-11-08 16:44:29 +0100

    blueprint moire

--- experiments/moire-spokes-animation-110717.py
+++ experiments/moire-spokes-animation-110717.py
@@ -4,32 +4,35 @@ def spokedCircle(x, y, radius, centerRadius, spokes, spokeWidth):
     translate(x, y)
     for spoke in range(spokes):
         # fill(0 + 1/spokes,0,0)
-        fill(randint(750,900)*.001,randint(500,686)*.001,.25) #.68627451, .588235294 
+        # fill(randint(750,900)*.001,randint(500,686)*.001,.25) #.68627451, .588235294 
+        fill(1,1,1)
         rect(0,centerRadius,spokeWidth,radius-centerRadius)
         rotate(360/spokes)
     restore()
 
 W, H = 1000, 1000
 
-frames = 50 # currently must be lower than number of maxSpokes
-rotations = 2
+frames = 70 # currently must be lower than number of maxSpokes
+rotations = 1
 totalSize = 1000
-spokes = 65
-maxSpokes = 75 # currently must be higher than number of frames
-minSpokes = 20
 
+maxSpokes = 500 # currently must be higher than number of frames
+minSpokes = 40
+spokes = minSpokes
 distance = 0
 maxDistance = 1000
 
-starterRadius = 1450
+starterRadius = 1200
 starterCenterRadius = 50
 
 radius = starterRadius
 centerRadius = starterCenterRadius
     
 incrementDistance = (maxDistance - distance)/frames
-incrRadius = ((totalSize-starterRadius)/frames)*4.5
-incrCenterRadius = ((totalSize-starterCenterRadius)/frames)*.25
+incrRadius = ((totalSize-starterRadius)/frames)*-2.5
+incrCenterRadius = ((totalSize-starterCenterRadius)/frames)*.55
+
+incrSpokes = int(ceil((maxSpokes - minSpokes)/frames))
 
 for i in range(frames):
     newPage(W, H)
@@ -40,8 +43,9 @@ for i in range(frames):
         distance += incrementDistance
         radius -= incrRadius
         centerRadius += incrCenterRadius
-        spokes += 3
-        fill(0.1,0.1,0.1)
+        spokes += incrSpokes
+        # fill(0.1,0.1,0.1)
+        fill(.2,.2,1)
         print spokes
     else:
         # print "going down " + str(i*((maxDistance - distance)/frames))
@@ -49,9 +53,10 @@ for i in range(frames):
         distance -= incrementDistance
         radius += incrRadius
         centerRadius -= incrCenterRadius
-        spokes -= 3
+        spokes -= incrSpokes
         print spokes
-    fill(0.1,0.1,0.1)
+    # fill(0.1,0.1,0.1)
+    fill(0,0,.7)
     rect(0,0,1000,1000)
     # radius = totalSize - i*(totalSize/frames)
     # centerRadius = -totalSize + i*(totalSize/frames)*2
@@ -61,7 +66,7 @@ for i in range(frames):
     # print spokes
     translate(500,500)
     # translate(500,500-centerRadius)
-    rotate(0 + i*((360*rotations/frames)))
+    rotate(0 - i*((360*rotations/frames)))
 
     # x, y, radius, centerRadius, spokes, spokeWidth
     spokedCircle(0, distance, radius, centerRadius, spokes, 1)
@@ -69,4 +74,4 @@ for i in range(frames):
 
     # translate(-500,-500+centerRadius)
 
-saveImage("exports/moire_21-110717.gif")
+saveImage("exports/moire_27-110717.gif")
